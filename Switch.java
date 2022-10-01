acd8503b9b4ff39a1290b71e0039949dd521ede0
Shubham Singh <104449952+ShubhamSinghgit@users.noreply.github.com>
2022-10-01 20:33:13 +0530

    Add files via upload

---
+++ Switch.java
@@ -0,0 +1,30 @@
+package shubham;
+
+import java.util.Scanner;
+
+public class Switch {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("enter fruit : ");
+        String fruit = in.next();
+
+        switch (fruit){
+            case "Mango":
+                System.out.println("King of fruits");
+                break;
+            case"Apple":
+                System.out.println("A Sweet red fruit");
+                break;
+            case"Orange":
+                System.out.println("A round fruit");
+                break;
+            case"Grapes":
+                System.out.println("Small round Fruit");
+                break;
+            default:
+                System.out.println("please enter a valid fruit");
+
+        }
+
+    }
+}
