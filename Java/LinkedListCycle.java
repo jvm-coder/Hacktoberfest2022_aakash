279cf08358e783f1d85bf3cbada59f557ec3db9c
Habimulla <114721840+Habimulla@users.noreply.github.com>
2022-10-02 20:37:42 +0530

    Create LinkedListCycle.java

---
+++ Java/LinkedListCycle.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if(head == null)
+            return false;
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while(slow  != null && fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast)
+                return true;
+        }
+        return false;
+    }
+}
