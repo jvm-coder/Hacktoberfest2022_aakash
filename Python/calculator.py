e99ed26095305e659c606f4e6f5f2d83edd77764
RohitChauhan13 <97402395+RohitChauhan13@users.noreply.github.com>
2022-10-07 23:55:23 +0530

    Added a simple calculator

---
+++ Python/calculator.py
@@ -0,0 +1,49 @@
+#Python program of creating a baisc calculator using functions
+
+def sum(a,b):
+    return a+b
+
+def subtract(a,b):
+    return a-b
+
+def multiply(a,b):
+    return a*b
+
+def divide(a,b):
+    return a/b
+
+
+print("Select operation you want to perform")
+print("1.Sum")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+
+while True:
+    # take input from the user
+    choice = input("Enter choice(1/2/3/4): ")
+
+    # check if choice is one of the four options
+    if choice in ('1', '2', '3', '4'):
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+
+        if choice == '1':
+            print(num1, "+", num2, "=", sum(num1, num2))
+
+        elif choice == '2':
+            print(num1, "-", num2, "=", subtract(num1, num2))
+
+        elif choice == '3':
+            print(num1, "*", num2, "=", multiply(num1, num2))
+
+        elif choice == '4':
+            print(num1, "/", num2, "=", divide(num1, num2))
+        
+        next_calculation = input("You want to do another calculation? (yes/no) ")
+        if next_calculation == "no":
+            print(" Congrats Your Calculations are completed")
+            break
+    
+    else:
+        print("Invalid Input")
