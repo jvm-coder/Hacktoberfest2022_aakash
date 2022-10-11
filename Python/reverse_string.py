c19540f24a880add8abfbe0b2c17e6b8e847d6d6
Kirill Stroganov <kiraplenkin@Kirills-MacBook-Air.local>
2022-10-11 22:12:40 +0300

    Add contribution for Hacktoberfest2022

---
+++ Python/reverse_string.py
@@ -0,0 +1,7 @@
+def reverseString(s) -> None:
+    l_idx = 0
+    r_idx = len(s) - 1
+    while l_idx < r_idx:
+        s[l_idx], s[r_idx] = s[r_idx], s[l_idx]
+        l_idx += 1
+        r_idx -= 1
