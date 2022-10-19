3b38ae9fdf8d3d9b2b60f60bfe61e2c4828367bc
Harshit Sharma <55287441+harshit-3103@users.noreply.github.com>
2022-10-19 12:25:54 +0530

    Add files via upload

---
+++ JavaScript(JS)/My Notes/index.js
@@ -0,0 +1,24 @@
+let a = [1,33,4,5];
+a.shift()
+console.log(a);
+
+
+// console.log('line number 1',a);
+// var a = 10;
+
+// function b(){
+//     console.log('line number 5',a);
+// }
+// console.log('line number 7',a);
+// function fn(){
+//     console.log('line number 9',a);
+//     var a = 20;
+//     b();
+//     console.log('line number 12',a);
+// }
+// fn();
+
+
+
+// Lexical scope and scope chain
+
